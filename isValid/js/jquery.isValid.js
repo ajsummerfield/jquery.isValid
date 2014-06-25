46e28148c2af7e450b108eb7f56360f7dabde74d
Andy Summerfield <aj.summerfield93@gmail.com>
2014-06-25 23:02:15 +0100

    Plugin rewritten in different pattern structure.

--- isValid/js/jquery.isValid.js
+++ isValid/js/jquery.isValid.js
@@ -1,108 +1,133 @@
-/* isValid Form Validation Plugin */
-/* Version 1.0 */
-/* Andy Summerfield */
+(function($) {
 
-(function ($, window, document, undefined) {
-
-    var self = null;
+    $.isValid = function(element, options) {
     
-    var IsValid = {
-        
-        init: function (options, elem) {
+        var defaults = {
+            username: {
+                minLength: 1,
+                maxLength: 200,
+                showLengthError: true,
+                lengthErrorMessage: "Username must be be at least 1 character long."
+            },
+            password: {
+                minLength: 1,
+                maxLength: 200,
+                numbers: false,
+                letters: true,
+                showLengthError: true,
+                lengthErrorMessage: "Password must be be at least 1 character long.",
+                showPasswordMatchError: false,
+                passwordMatchErrorMessage: "Password must contain letters."
+            },
+            email: {
+                domain: ''
+            },
+            letters: {
+                showMatchError: true,
+                matchErrorMessage: "Field is letters only."
+            },
+            numbers: {
+                showMatchError: true,
+                matchErrorMessage: "Field is numbers only."
+            },
+            postcode: {
+                showMatchError: true     
+            }
+        }
             
-            self = this;
-
-            self.elem = elem;
-            self.$elem = $(elem);
+        var self = this;
+        
+        self.init = function() {
+            
+            self.elem = element;
+            self.$elem = $(element);
             self.isFormValid = false;
             self.isValid = false;
             self.formID = "#" + self.$elem.attr('id');
             self.$elem.find(':submit').addClass('submit-btn');
             
-            self.options = $.extend(true, $.fn.isValid.options, options);
+            self.options = $.extend({}, defaults, options);
             
             self.formArray = $(self.formID + ' :input[type="text"],' + self.formID + ' :input[type="password"],' + self.formID + ' :input[type="tel"],' + self.formID + ' textarea,' + self.formID + ' select');
             
             self.formArray.each(function (index, field) {
-                IsValid.createErrorMessage(field);
+                self.createErrorMessage(field);
             });
-            
-            return self;
-        },
+        }
         
-        isFormValid: function() {
+        self.isFormValidated = function() {
             return ($('.invalid').length) ? false : true;
-        },
-        
-        isValidField: function (field) {
+        }
+  
+        self.isValidField = function(field) {
             
-            if (field.disabled || $(field).hasClass('not-required')) {
+            if (field.disabled || $(field).attr('data-field-info') === "notrequired") {
                 self.isValid = true;
             } else {
-                if(IsValid.isEmpty(field)) {
-                    IsValid.invalidAction(field);
+                if(self.isEmpty(field)) {
+                    invalidAction(field);
                 } else {
                     
-                    var data = ($(field).attr('data-field-info') !== undefined) ? $(field).attr('data-field-info').toLowerCase() : "";
+                    var data = changeToLowercase(field);
                     
                     switch(data) {
                         case 'username':
-                            self.isValid = (IsValid.isUsernameValid(field)) ? IsValid.validAction(field) : IsValid.invalidAction(field);
+                            self.isValid = completeAction((self.isUsernameValid(field)), field);
                             break;
                            
                         case 'password':
-                            self.isValid = (IsValid.isPasswordValid(field)) ? IsValid.validAction(field) : IsValid.invalidAction(field);
+                            self.isValid = completeAction((self.isPasswordValid(field)), field);
                             break; 
                            
                         case 'email':
-                            self.isValid = (IsValid.isEmailValid(field)) ? IsValid.validAction(field) : IsValid.invalidAction(field);
+                            self.isValid = completeAction((self.isEmailValid(field)), field);
                             break;
                         
                         case 'dateofbirth':
-                            self.isValid = (IsValid.isDateOfBirthValid(field)) ? IsValid.validAction(field) : IsValid.invalidAction(field);
+                            self.isValid = completeAction((self.isDateOfBirthValid(field)), field);
                             break;
                         
-                        case 'numbers':
-                            self.isValid = (IsValid.isNumbers(field)) ? IsValid.validAction(field) : IsValid.invalidAction(field);
-                            break;
-
                         case 'letters':
-                            self.isValid = (IsValid.isLetters(field)) ? IsValid.validAction(field) : IsValid.invalidAction(field);
+                            self.isValid = completeAction((self.isLetters(field)), field);
+                            break;
+                            
+                        case 'numbers':
+                            self.isValid = completeAction((self.isNumbers(field)), field);
                             break;
                             
                         case 'postcode':
-                            self.isValid = (IsValid.isPostCodeValid(field)) ? IsValid.validAction(field) : IsValid.invalidAction(field);
+                            self.isValid = completeAction((self.isPostCodeValid(field)), field);
                             break;
                             
                         default:
-                            IsValid.validAction(field);
+                            validAction(field);
                             self.isValid = true;
                     }
                 }
             }
             
             return self.isValid;
-        },
+        }
         
-        isEmpty: function (field) {
+        self.isEmpty = function(field) {
             return ($(field).val() === "");
-        },
+        }
         
-        isLetters: function(field) {
+        self.isLetters = function(field) {
             var letterMatcher = /^[A-Za-z ]+$/;
             return (letterMatcher.test($(field).val()));
-        },
+        }
         
-        isNumbers: function(field) {
+        self.isNumbers = function(field) {
             var numberMatcher = /^[+-]?[0-9]{1,9}(?:\.[0-9]{1,2})?$/;
             return (numberMatcher.test($(field).val()));
-        },
+        }
         
-        isUsernameValid: function (field) {
+        self.isUsernameValid = function(field) {
             return (($(field).val().length >= self.options.username.minLength) && ($(field).val().length <= self.options.username.maxLength));
-        },
+        }
         
-        isPasswordValid: function (field) {
+        self.isPasswordValid = function(field) {
             var passwordMatcher = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
             
             if (self.options.password.numbers && self.options.password.letters) { 
@@ -110,9 +135,9 @@
             } else {
                 return (($(field).val().length >= self.options.password.minLength) && ($(field).val().length <= self.options.password.maxLength));
             }
-        },
+        }
         
-        isDateOfBirthValid: function (field) {
+        self.isDateOfBirthValid = function(field) {
             var date = $(field).val();
             var count = date.match(/\//g);
             
@@ -121,10 +146,10 @@
             } else {
                 var data = date.split('/');
                 return (Date.parse(data[2] + "-" + data[1] + "-" + data[0]) > 0);
-            }
-        },
+            }  
+        }
         
-        isEmailValid: function (field) {
+        self.isEmailValid = function(field) {
             var emailMatcher = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             
             if(self.options.email.domain === '') {
@@ -132,15 +157,15 @@
             } else {
                 return ((emailMatcher.test($(field).val())) && (($(field).val().indexOf(self.options.email.domain, $(field).val().length - self.options.email.domain.length) !== -1)));
             }
-        },
+        }
         
-        isPostCodeValid: function(field) {
+        self.isPostCodeValid = function(field) {
             return (checkPostCode($(field).val()));
-        },
-
-        createErrorMessage: function (field) {
+        }
+        
+        self.createErrorMessage = function(field) {
             
-            var type = IsValid.changeToLowercase(field);
+            var type = changeToLowercase(field);
             
             switch(type) {
             
@@ -178,14 +203,22 @@
                     
                         break;
             }
-            
-        },
-
-        validAction: function (field) {
+        }
+        
+        // Private Methods
+        var changeToLowercase = function(field) {
+            return ($(field).attr('data-field-info') !== undefined) ? $(field).attr('data-field-info').toLowerCase() : "";
+        }
+        
+        var completeAction = function(isValid, field) {
+            (isValid) ? validAction(field) : invalidAction(field);
+        }
+        
+        var validAction = function(field) {
             
             $(field).removeClass('invalid');
             
-            var type = IsValid.changeToLowercase(field);
+            var type = changeToLowercase(field);
                     
             switch(type) {
                     
@@ -203,13 +236,13 @@
             }
             
             return true;
-        },
-    
-        invalidAction: function (field) {
-            
+        }
+        
+        var invalidAction = function(field) {
+        
             $(field).addClass('invalid');
             
-            var type = IsValid.changeToLowercase(field);
+            var type = changeToLowercase(field);
                     
             switch(type) {
                     
@@ -223,87 +256,35 @@
             }
             
             return false;
-        },
-            
-        changeToLowercase: function (field) {
-        
-            return ($(field).attr('data-field-info') !== undefined) ? $(field).attr('data-field-info').toLowerCase() : "";
         }
         
-    };
-
-    $.fn.isValid = function (options) {
+        self.init();
+  
+    }
         
-        var opts = $.extend({}, $.fn.isValid.options, options);
-        
-        return this.each(function () {
+    $.fn.isValid = function(options) {
+    
+        return this.each(function() {
+    
+            var newIsValid = new $.isValid(this, options);
+            $(this).data('isValid', newIsValid);
             
-            var isvalid = Object.create(IsValid);
-
-            var self = isvalid.init(opts, this);
-
-            $.data(this, 'isValid', isvalid);
-            
-            $(self.formID + ' :input[type="submit"]').click(function (e) {
-                self.formArray.each(function (index, field) {
-                    self.isValid = IsValid.isValidField(field);
-                    self.isFormValid = IsValid.isFormValid();
-                    if (!self.isFormValid) {
+            $(newIsValid.formID + ' :input[type="submit"]').click(function (e) {
+                newIsValid.formArray.each(function (index, field) {
+                    newIsValid.isValid = newIsValid.isValidField(field);
+                    newIsValid.isFormValid = newIsValid.isFormValidated();
+                    if (!newIsValid.isFormValid) {
                         e.preventDefault();
                     }
                 });
             });
 			
-            self.formArray.each(function (index, field) {
+            newIsValid.formArray.each(function (index, field) {
                 $(field).blur(function() {
-                    self.isValid = IsValid.isValidField(field);
+                    newIsValid.isValid = newIsValid.isValidField(field);
                 });
             });
-
         });
-    };
-
-    $.fn.isValid.options = {
-        username: {
-            minLength: 1,
-            maxLength: 200,
-            showLengthError: true,
-            lengthErrorMessage: "Username must be be at least 1 character long."
-        },
-        password: {
-            minLength: 1,
-            maxLength: 200,
-            numbers: false,
-            letters: true,
-            showLengthError: true,
-            lengthErrorMessage: "Password must be be at least 1 character long.",
-            showPasswordMatchError: false,
-            passwordMatchErrorMessage: "Password must contain letters."
-        },
-        email: {
-            domain: ''
-        },
-        letters: {
-            showMatchError: true,
-            matchErrorMessage: "Field is letters only."
-        },
-        numbers: {
-            showMatchError: true,
-            matchErrorMessage: "Field is numbers only."
-        },
-        postcode: {
-            showMatchError: true,
-            matchErrorMessage: "Not a valid Post Code."
-        }
-    };
-
-})(jQuery, window, document); 
-
-// Utility
-if (typeof Object.create !== 'function') {
-    Object.create = function (obj) {
-        function F() { };
-        F.prototype = obj;
-        return new F();
-    };
-}
+    }
+  
+})(jQuery);

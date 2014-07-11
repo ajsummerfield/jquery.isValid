f913c9f4662e9ceec2e58de323fd5731bbf0a5c7
Andy Summerfield <aj.summerfield93@gmail.com>
2014-07-11 18:16:36 +0100

    Adding in grunt, to trial it out and learn more about it.

---
+++ isValid/Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        cssmin: {
+            minify: {
+                expand: true,
+                src: ['css/jquery.isValid.css', '!*.min.css'],
+                ext: 'isValid.min.css'
+            }
+        },
+
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'js/<%= pkg.name %>.js',
+                dest: 'js/<%= pkg.name %>.min.js'
+            }
+        },
+        
+        jshint: {
+            src: ['js/*.js'],
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: false,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                  alert: true,
+                  require: true,
+                  define: true,
+                  requirejs: true,
+                  describe: true,
+                  expect: true,
+                  it: true,
+                  L: true
+                }
+            }
+        },
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify', 'cssmin']);
+
+};

bb231dd3548bd0918cfcf6017ad28226d83d6614
AjDrummer <aj.summerfield93@gmail.com>
2015-08-12 17:41:19 +0100

    Updating documentation to include new options, changes and validator stuff.

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
     $('form').submit(function(event) {
         return false;
     });
-    
+
     $('#simple-form').isValid();
-    
+
     $('#complex-form').isValid({
         password: {
             numbers: true,
@@ -20,6 +20,35 @@ $(document).ready(function() {
         date: {
             allowFutureDates: false,
             invalidErrorMessage: 'Invalid Date entered'
+        }
+    });
+
+    $('#validator-form').isValid({
+        fieldTypes: {
+            idNumber: {
+                requiredErrorMessage: 'Please specify your ID Number',
+                formatErrorMessage: 'ID Number is incorrect format',
+                callbacks: {
+                    onValidated: function(event) {
+                        console.log(event);
+                    }
+                }
+            }
+        },
+        validators: {
+            idNumber: {
+                name: 'isIDNumberValid',
+                validate: function(field) {
+
+                    var validResult = field.val().length === 6 && /^[0-9]+$/.test(field.val()),
+                        errorType = validResult ? '' : 'format';
+
+                    return {
+                        isValid: validResult,
+                        activeErrorType: errorType
+                    };
+                }
+            }
         }
     });
 });

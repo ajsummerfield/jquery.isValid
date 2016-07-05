2f42a0538c9a230efdbe10a8bc57250ebacd658a
AjDrummer93 <aj.summerfield93@gmail.com>
2016-07-05 23:18:52 +0100

    Showing/hiding of the error container on blur/keyup events and other refactors to current code

--- Web/js/jIsValid.js
+++ Web/js/jIsValid.js
@@ -13,42 +13,74 @@ jIsValid.prototype = {
         this.formId = this.element.id;
         this.formElements = document.getElementsByTagName('input');
         this.element.addEventListener('submit', function(e) {
-            self.validateForm(e);
+            var isValid = self.validateForm(e);
+            
+            if (!isValid) {
+                e.preventDefault();
+                return false;
+            }
         });
+
+        for (var i = 0; i < this.formElements.length; i++) {
+            var el = this.formElements[i];
+
+            el.addEventListener('blur', function(e) {
+                self.runValidation(el);
+            });
+
+            el.addEventListener('keyup', function(e) {
+                self.runValidation(el);
+            });
+        }
     },
 
     validateForm: function(e) {
         for (var i = 0; i < this.formElements.length; i++) {
             var el = this.formElements[i];
-            var result = this.isValidField(el);
-            if (result.isValid) {
-                this.hideError(el);
-            } else {
-                this.showError(el);
-            }
+            this.runValidation(el);
         }
 
         return this.isFormValid();
     },
 
-    isValidField: function(element) {
-        var fieldType = element.attributes['data-field-type'].value;
-        return this.validators.isGeneralValid(element);
-    },
-
-    isFormValid: function() {
-        for (var i = 0; i < this.element.children; i++) {
-            var child = this.element.children[i];
-            
+    runValidation: function(el) {
+        var result = this.isValidField(el);
+        if (result.isValid) {
+            this.hideError(el);
+        } else {
+            this.showError(el);
         }
     },
 
-    hideError: function(el) {
+    isValidField: function(el) {
+        var fieldType = el.attributes['data-field-type'].value;
+        return this.validators.isGeneralValid(el);
+    },
 
+    isFormValid: function() {
+        return this.element.querySelectorAll('.invalid').length === 0;
+    },
+
+    hideError: function(el) {
+        var errorContainer = el.parentElement.querySelector('.error-message');
+        el.classList.remove('invalid');
+
+        if (errorContainer) {
+            errorContainer.remove();
+        }
     },
 
     showError: function(el) {
+        var errorContainer = el.parentElement.querySelector('.error-message');
 
+        if (errorContainer) {
+            errorContainer = this._updateErrorContainer(el, errorContainer);
+        } else {
+            errorContainer = this._createErrorContainer(el);
+        }
+
+        el.parentElement.appendChild(errorContainer);
+        el.classList.add('invalid');
     },
 
     validators: {
@@ -57,8 +89,25 @@ jIsValid.prototype = {
                 errorType = isEmpty ? 'required' : '';
 
             return {
-                isValid: !isEmpty
+                isValid: !isEmpty,
+                errorType: errorType
             };
         }
+    },
+
+    _createErrorContainer: function(el) {
+        var errorContainer = document.createElement('div');
+        var errorMessage = el.attributes['data-error-message'].value;
+        errorContainer.setAttribute('class', 'error-message');
+        errorContainer.innerHTML = errorMessage;
+
+        return errorContainer;
+    },
+
+    _updateErrorContainer: function(el, errorContainer) {
+        var errorMessage = el.attributes['data-error-message'].value;
+        errorContainer.innerHTML = errorMessage;
+
+        return errorContainer;
     }
 };
